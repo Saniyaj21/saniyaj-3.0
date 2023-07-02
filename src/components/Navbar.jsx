e83c9e80e83938bf160708b3ef512e0370cf5113
saniyaj <saniyajmallik12@gmail.com>
2023-07-02 22:42:57 +0530

    Navbar semi complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React from 'react';
+import '../styles/navbar.scss';
 
 const Navbar = () => {
+
+    const nav_active = document.querySelectorAll('.navbar');
+
+    const open_menu = () => {
+        nav_active.forEach((elem) => elem.classList.add('active'))
+    }
+
+    const close_nav = () => {
+        nav_active.forEach(elem => elem.classList.remove('active'))
+    }
+
     return (
-        <nav>Navbar</nav>
+
+        <header>
+            <div class="logo">
+                <a href="#">Saniyaj Mallik</a>
+            </div>
+
+            <div class="menu">
+                <i class="fa-sharp fa-solid fa-bars" onClick={open_menu}></i>
+                <div class="navbar nav-first">
+                    <div class="navbar nav-second">
+                        <div class="navbar navbar-list">
+                            <i class="fa-solid fa-square-xmark" onClick={close_nav}></i>
+
+                            <ul>
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#" >about</a></li>
+                                <li><a href="#">services</a></li>
+                                <li><a href="#">contact</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
     )
 }
 
