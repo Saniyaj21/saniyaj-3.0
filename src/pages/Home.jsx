bc18969fb79054673e8d6b7f0e1935078acb867e
saniyaj <saniyajmallik12@gmail.com>
2023-07-06 10:52:00 +0530

    Favicon, type writer added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,8 @@ import Skills from '../pages/Skills';
 import Qualification from '../pages/Qualification';
 import Contact from '../pages/Contact';
 
+import { Typewriter } from 'react-simple-typewriter'
+
 // import { motion } from "framer-motion";
 
 
@@ -14,8 +16,27 @@ const Home = () => {
     return (
         <div className='home'>
             <section id='home-section'>
-                <h3>Home</h3>
+                <h1 className='writer'>I am a{" "}
+                    <span>
+                        <Typewriter
+                            words={['Developer', 'Designer', "Student", 'Explainer']}
+                            loop={true}
+                            cursor
+                            cursorStyle='|'
+                            typeSpeed={50}
+                        // deleteSpeed={50}
+                        // delaySpeed={1000}
+
+                        />
+                    </span>
+                </h1>
+
+
             </section>
+
+
+
+
             <section id='about-section'>
                 <About />
             </section>
