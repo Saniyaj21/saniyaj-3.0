1dc39bcbdcd55b6e9a5cd3cb63c0b203f15570bf
saniyaj mallik <saniyajmallik12@gmail.com>
2023-09-13 08:14:27 +0530

    loading skeleton

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -4,9 +4,29 @@ import ReactLoading from "react-loading";
 
 const Loading = () => {
 	return (
-		<div className='loading-container'>
-			<div class='circle'></div>
+
+
+		<div className="container-loading">
+			<div className="text-box">
+				<div className="hi util"></div>
+				<div className="name util"></div>
+				<div className="writer util"></div>
+
+				<div className="btn-group">
+					<div className="btn-loading util"></div>
+					<div className="btn-loading util"></div>
+				</div>
+			</div>
+			<div className="image-box">
+				<div className="img util"></div>
+			</div>
 		</div>
+
+
+
+		// <div className='loading-container'>
+		// 	<div class='circle'></div>
+		// </div>
 
 		// <div className="load-screen">
 		//     <ReactLoading type={'bars'} color={'blue'} height={67} width={75} />
